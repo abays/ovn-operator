a1aa6bc73e1de56280b7918b55d62aabfcbcff49
Andrew Bays <abays@redhat.com>
2022-10-11 12:05:23 +0000

    OVN NorthD

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,21 +17,12 @@ limitations under the License.
 package controllers
 
 import (
-	// "path/filepath"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	// "k8s.io/client-go/kubernetes/scheme"
-	// "k8s.io/client-go/rest"
-	// "sigs.k8s.io/controller-runtime/pkg/client"
-	// "sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
-	//	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	//	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	// ovnv1alpha1 "github.com/openstack-k8s-operators/ovn-operator/api/v1alpha1"
-	// +kubebuilder:scaffold:imports
+	//+kubebuilder:scaffold:imports
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
@@ -45,45 +36,43 @@ func TestAPIs(t *testing.T) {
 		[]Reporter{printer.NewlineReporter{}})
 }
 
-/*
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+// var cfg *rest.Config
+// var k8sClient client.Client
+// var testEnv *envtest.Environment
 
-var _ = BeforeSuite(func(done Done) {
-	// logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+// var _ = BeforeSuite(func(done Done) {
+// 	// logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
 
-	By("bootstrapping test environment")
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
-	}
+// 	By("bootstrapping test environment")
+// 	testEnv = &envtest.Environment{
+// 		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
+// 	}
 
-	var err error
-	cfg, err = testEnv.Start()
-	Expect(err).ToNot(HaveOccurred())
-	Expect(cfg).ToNot(BeNil())
+// 	var err error
+// 	cfg, err = testEnv.Start()
+// 	Expect(err).ToNot(HaveOccurred())
+// 	Expect(cfg).ToNot(BeNil())
 
-	err = ovnv1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
+// 	err = ovnv1alpha1.AddToScheme(scheme.Scheme)
+// 	Expect(err).NotTo(HaveOccurred())
 
-	err = ovnv1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
+// 	err = ovnv1alpha1.AddToScheme(scheme.Scheme)
+// 	Expect(err).NotTo(HaveOccurred())
 
-	err = ovnv1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
+// 	err = ovnv1alpha1.AddToScheme(scheme.Scheme)
+// 	Expect(err).NotTo(HaveOccurred())
 
-	// +kubebuilder:scaffold:scheme
+// 	// +kubebuilder:scaffold:scheme
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).ToNot(HaveOccurred())
-	Expect(k8sClient).ToNot(BeNil())
+// 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+// 	Expect(err).ToNot(HaveOccurred())
+// 	Expect(k8sClient).ToNot(BeNil())
 
-	close(done)
-}, 60)
+// 	close(done)
+// }, 60)
 
-var _ = AfterSuite(func() {
-	By("tearing down the test environment")
-	err := testEnv.Stop()
-	Expect(err).ToNot(HaveOccurred())
-})
-*/
+// var _ = AfterSuite(func() {
+// 	By("tearing down the test environment")
+// 	err := testEnv.Stop()
+// 	Expect(err).ToNot(HaveOccurred())
+// })
